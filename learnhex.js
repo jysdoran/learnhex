eb06c8b115b71285004921ab441ba233b579b2dd
Robert Sesek <rsesek@google.com>
2012-11-29 10:54:23 -0500

    Create a simple Angluar app that creates a 10x10 grid using Bootstrap

---
+++ learnhex.js
@@ -0,0 +1,30 @@
+/**
+ * Learn HEX Math
+ * Copyright 2012 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var NUM_ROWS = 10;
+var NUM_COLS = 10;
+
+function ProblemsController($scope) {
+  $scope.rows = [];
+  for (var i = 0; i < NUM_ROWS; ++i) {
+    var row = [];
+    for (var j = 0; j < NUM_COLS; ++j) {
+      row.push("hello (" + i + "," + j + ")");
+    }
+    $scope.rows.push(row);
+  }
+}
